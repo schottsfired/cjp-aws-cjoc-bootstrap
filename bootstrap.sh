b480c63d54444ce2299860af873da0551dbae0b9
David Schott <schottdb@gmail.com>
2015-12-18 09:14:40 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,12 @@
-
-
 #associate this instance with an elastic IP
 aws ec2 associate-address --allocation-id eipalloc-01b26365
+
+# Update hosts file
+echo "127.0.0.1 $(hostname)" >> /etc/hosts
+
+# Update packages
+sudo apt-get update
+sudo apt-get upgrade
 
 # set JENKINS_HOME
 # ...
