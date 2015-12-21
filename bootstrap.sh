376ad3d13d585fe0473addd3d2dfa5cd54be149d
David Schott <schottdb@gmail.com>
2015-12-21 16:24:02 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,3 @@
-# Global config
-export AWS_ACCESS_KEY_ID=myId
-export AWS_SECRET_ACCESS_KEY=wouldntyouliketoknow
-export AWS_DEFAULT_REGION=us-east-1
-
 # Update hosts file (with private IP?)
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
 
@@ -11,7 +6,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 #associate this instance with an elastic IP (need inet access first)
-aws ec2 associate-address --allocation-id eipalloc-01b26365
+#aws ec2 associate-address --allocation-id eipalloc-01b26365
 
 # set JENKINS_HOME
 # ...
