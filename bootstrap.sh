dccd06ce16519859de57ec3f2c426747a7134e27
David Schott <schottdb@gmail.com>
2015-12-21 16:39:19 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ echo "127.0.0.1 $(hostname)" >> /etc/hosts
 
 # Update packages
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
 #associate this instance with an elastic IP (need inet access first)
 #aws ec2 associate-address --allocation-id eipalloc-01b26365
