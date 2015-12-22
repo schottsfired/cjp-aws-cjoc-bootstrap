c81ef20c81a6d92c5d47a8929e8db87d6a3ac43a
David Schott <schottdb@gmail.com>
2015-12-22 13:55:44 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,16 @@
 # Update hosts file (with private IP?)
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
 
-# Update packages
+# Update packages (several minutes)
 #sudo apt-get update
 #sudo apt-get -y upgrade
 
+#install AWS CLI (1-2 mins)
+sudo apt-get -y install awscli
+
 #associate this instance with an elastic IP
-#aws ec2 associate-address --allocation-id eipalloc-01b26365
+MY_INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+aws ec2 associate-address --region us-east-1 --instance-id "$MY_INSTANCE_ID" --allocation-id eipalloc-01b26365
 
 # set JENKINS_HOME
 # ...
