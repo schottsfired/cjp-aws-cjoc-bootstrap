442c3fc570f2fcac80b78308f94697c0a6975fc0
David Schott <schottdb@gmail.com>
2015-12-23 10:38:54 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,8 +12,14 @@ sudo apt-get -y install awscli
 MY_INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 aws ec2 associate-address --region us-east-1 --instance-id "$MY_INSTANCE_ID" --allocation-id eipalloc-01b26365
 
+#mount NFS storage
+sudo apt-get -y install nfs-common
+sudo mkdir /var/lib/cjoc
+sudo mount -t nfs 10.0.2.83:/var/lib/cjoc /var/lib/cjoc
+#TODO use elastic IP
+
 # set JENKINS_HOME
-# ...
+export JENKINS_HOME=/var/lib/cjoc
 
 # restart Jenkins
-# ...
+sudo service jenkins-oc restart
